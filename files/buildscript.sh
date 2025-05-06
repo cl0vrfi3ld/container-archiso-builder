b883b9c92142f0b949817e40911d6480f1833d6d
cl0vrfi3ld <47996003+cl0vrfi3ld@users.noreply.github.com>
2025-05-06 00:30:24 -0400

    update script with new regex and out dir and input method

--- files/buildscript.sh
+++ files/buildscript.sh
@@ -3,19 +3,19 @@
 #Placing terminal to the starting folder
 cd /root/
 
-#Ask the user where are the archiso files
-echo "Type the repo URL where the archiso files are, followed by [ENTER]"
-echo "Example: https://github.com/nlhomme/lhoslite"
-read repository
+# #Ask the user where are the archiso files
+# echo "Type the repo URL where the archiso files are, followed by [ENTER]"
+# echo "Example: https://github.com/nlhomme/lhoslite"
+# read repository
 
-#Clone the lhos repo
-git clone $repository
+#Clone the provided repo
+git clone $@
 
 #Go to this new working folder
-cd $(ls|grep -Ev 'buildscript.sh')
+cd $(echo $@|grep -oP '/([A-Za-z0-9]+\.[A-Za-z]+\/[A-Za-z0-9]+\/)\K([A-Za-z0-9]+(?:-[A-Za-z0-9]+)+)(?=\.git)')
 
 #Run the script to build the ISO
 ./build.sh -v
 
 #Move the final ISO to /tmp to be accessible from the host
-mv out/*.iso /tmp
+mv out/*.iso /out
